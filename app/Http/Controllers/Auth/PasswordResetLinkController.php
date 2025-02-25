c23ce1d8d61a7169bc4e61838d5242086ee6825f
amanah-teknologi-digital <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>
2025-02-25 23:37:07 +0700

    update lupa password

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -39,6 +39,6 @@ class PasswordResetLinkController extends Controller
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
-                        ->withErrors(['email' => __($status)]);
+                        ->withErrors(['email' => 'Email tidak ditemukan, silahkan coba lagi!']);
     }
 }
--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,7 +4,8 @@
 
 @section('page-style')
     @vite([
-      'resources/assets/vendor/scss/pages/page-auth.scss'
+      'resources/assets/vendor/scss/pages/page-auth.scss',
+      'resources/assets/css/custom.scss'
     ])
 @endsection
 
@@ -17,26 +18,48 @@
                 <div class="card px-sm-6 px-0">
                     <div class="card-body">
                         <!-- Logo -->
-                        <div class="app-brand justify-content-center mb-6">
-                            <a href="{{url('/')}}" class="app-brand-link gap-2">
-                                <span class="app-brand-logo demo">@include('_partials.macros',["width"=>25,"withbg"=>'var(--bs-primary)'])</span>
-                                <span class="app-brand-text demo text-heading fw-bold">{{ config('variables.templateName') }}</span>
+                        <div class="app-brand justify-content-center">
+                            <a href="{{url('/')}}" class="logo d-flex align-items-center me-auto me-xl-0 order-first">
+                                <img src="{{ asset('landing_page_rss/teknikgeo.png') }}" alt="">
+                                <h1 class="sitename">GeoReserve</h1>
                             </a>
                         </div>
+
                         <!-- /Logo -->
-                        <h4 class="mb-1">Forgot Password? 🔒</h4>
-                        <p class="mb-6">Enter your email and we'll send you instructions to reset your password</p>
+                        <h4 class="mb-1">Lupa Password? 🔒</h4>
+                        <p class="mb-6">Masukan email anda dan kami akan mengirimkan link intruksi untuk mereset password anda.</p>
+                        @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                                <div class="alert alert-danger alert-dismissible" role="alert">
+                                    {{ $error }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            @endforeach
+                        @endif
+                        @if (session('error'))
+                            <div class="alert alert-danger alert-dismissible" role="alert">
+                                {{ session('error') }}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        @endif
+                        @if (session('status'))
+                            <div class="alert alert-success alert-dismissible" role="alert">
+                                Link reset password sudah dikirim ke alamat email anda, cek dan buka link tersebut untuk mereset password anda.
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        @endif
                         <form id="formAuthentication" class="mb-6" action="{{ route('password.email') }}" method="POST">
+                            @csrf
                             <div class="mb-6">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" autofocus>
+                                <input type="text" class="form-control" id="email" name="email" placeholder="Masukan Email" autofocus>
                             </div>
-                            <button class="btn btn-primary d-grid w-100">Send Reset Link</button>
+                            <button class="btn btn-primary d-grid w-100">Kirim Reset Link</button>
                         </form>
                         <div class="text-center">
                             <a href="{{ route('login') }}" class="d-flex justify-content-center">
                                 <i class="bx bx-chevron-left scaleX-n1-rtl me-1"></i>
-                                Back to login
+                                Kembali ke halaman Login
                             </a>
                         </div>
                     </div>
@@ -45,4 +68,10 @@
             </div>
         </div>
     </div>
+@endsection
+@section('page-script')
+    @vite([
+        'resources/assets/vendor/libs/jquery-validation/jquery.validate.js',
+        'resources/views/auth/js/lupa_password.js',
+    ])
 @endsection
