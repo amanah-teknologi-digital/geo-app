8d6aa566efbb54e3fcd8178e4af915d5422f095f
fajar-dwi <56991659+fajar-dwi@users.noreply.github.com>
2025-02-26 12:02:35 +0700

    update error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts/blankLayout')
+
+@section('title', 'Under Maintenance'.' • '.config('variables.templateName'))
+
+@section('page-style')
+    <!-- Page -->
+    @vite(['resources/assets/vendor/scss/pages/page-misc.scss'])
+@endsection
+
+@section('content')
+    <!--Under Maintenance -->
+    <div class="container-xxl container-p-y">
+        <div class="misc-wrapper">
+            <h3 class="mb-2 mx-2">Aplikasi masih dalam perbaikan! 🚧</h3>
+            <p class="mb-6 mx-2">
+                Maaf atas ketidaknyamanan ini, kami sedang melakukan perbaikan pada saat ini
+            </p>
+            <a href="{{url('/')}}" class="btn btn-primary">Kembali ke landing page</a>
+            <div class="mt-6">
+                <img src="{{asset('assets/img/illustrations/girl-doing-yoga-light.png')}}" alt="girl-doing-yoga-light" width="500" class="img-fluid">
+            </div>
+        </div>
+    </div>
+    <!-- /Under Maintenance -->
+@endsection
