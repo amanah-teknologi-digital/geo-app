f34c7fa49485c71bf837788de09a7ee4067bcaa8
amanah-teknologi-digital <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>
2025-03-02 10:33:33 +0700

    update dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -83,40 +83,8 @@
     </div>
 @endsection
 
-<script>
-    $(document).ready(function () {
-        $("#formAuthentication").validate({
-            rules: {
-                email: {
-                    required: true,
-                    email: true
-                },
-                password: {
-                    required: true,
-                }
-            },
-            messages: {
-                email: {
-                    required: "Email wajib diisi",
-                    email: "Format email tidak valid"
-                },
-                password: {
-                    required: "Password wajib diisi",
-                }
-            },
-            errorPlacement: function(error, element) {
-                // Menentukan lokasi error berdasarkan id atau atribut lain
-                if (element.attr("name") === "password") {
-                    error.appendTo("#error-password");
-                } else {
-                    // Default: tampilkan setelah elemen
-                    error.insertAfter(element);
-                }
-            },
-            submitHandler: function (form) {
-                form.submit();
-            }
-        });
-    })
-</script>
-
+@section('page-script')
+    @vite([
+        'resources/views/auth/js/login.js'
+    ]);
+@endsection
