faff76f254e8f3c5006c51a467c00ed89f297858
amanah-teknologi-digital <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>
2025-03-10 09:25:17 +0700

    update tambah pengumuman

--- app/Http/Controllers/PengumumanController.php
+++ app/Http/Controllers/PengumumanController.php
@@ -43,19 +43,16 @@ class PengumumanController extends Controller
                     return $data_pengumuman->is_posting? '<span class="badge bg-sm text-success">Posting</span>':'<span class="badge bg-sm text-warning">Tidak</span>';
                 })
                 ->addColumn('aksi', function ($data_pengumuman) {
-                    $html = '<a href="'.route('pengumuman.edit', $data_pengumuman->id_pengumuman).'" class="btn btn-sm py-0 px-2 btn-primary"><span class="bx bx-edit-alt"></span><span class="d-none d-lg-inline-block">&nbsp;Edit</span></a>&nbsp;';
+                    $html = '<a href="'.route('pengumuman.edit', $data_pengumuman->id_pengumuman).'" class="btn btn-sm py-1 px-2 btn-primary"><span class="bx bx-edit-alt"></span><span class="d-none d-lg-inline-block">&nbsp;Edit</span></a>&nbsp;';
                     $html .= '<div class="d-inline-block"><a href="javascript:;" class="btn btn-icon dropdown-toggle hide-arrow me-1" data-bs-toggle="dropdown" aria-expanded="false"><i class="bx bx-dots-vertical-rounded icon-base"></i></a>';
                     $html .= '<div class="dropdown-menu dropdown-menu-end m-0" style="">';
                     if ($data_pengumuman->is_posting == 1) {
                         $html .= '<a href="javascript:;" class="dropdown-item text-warning batal-posting" data-id="'.$data_pengumuman->id_pengumuman.'" data-bs-toggle="modal" data-bs-target="#modal-unpost"><span class="bx bx-candles"></span>&nbsp;Unposting</a>';
-                        //$html .= '<button class="dropdown-item btn btn-sm btn-warning batal-posting" data-id="'.$data_pengumuman->id_pengumuman.'" data-bs-toggle="modal" data-bs-target="#modal-unpost"><span class="bx bx-candles"></span><span class="d-none d-lg-inline-block">&nbsp;Unposting</span></button>';
                     }else{
                         $html .= '<a href="javascript:;" class="dropdown-item text-success posting-pengumuman" data-id="'.$data_pengumuman->id_pengumuman.'" data-bs-toggle="modal" data-bs-target="#modal-post"><span class="bx bx-paper-plane"></span>&nbsp;Posting</a>';
-                        //$html .= '<button class="dropdown-item btn btn-sm btn-success posting-pengumuman" data-id="'.$data_pengumuman->id_pengumuman.'" data-bs-toggle="modal" data-bs-target="#modal-post"><span class="bx bx-paper-plane"></span><span class="d-none d-lg-inline-block">&nbsp;Posting</span></button>';
                     }
                     $html .= '<div class="dropdown-divider"></div>';
                     $html .= '<a href="javascript:;" class="dropdown-item text-danger delete-record" data-id="'.$data_pengumuman->id_pengumuman.'" data-bs-toggle="modal" data-bs-target="#modal-hapus"><span class="bx bx-trash"></span>&nbsp;Hapus</a>';
-                    //$html .= '<button class="dropdown-item btn btn-sm btn-danger delete-pengumuman" ><span class="bx bx-trash"></span><span class="d-none d-lg-inline-block">&nbsp;Hapus</span></button>&nbsp;';
                     $html .= '</div></div>';
                     return $html;
                 })
